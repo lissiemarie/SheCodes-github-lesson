d5317dbc8228f6d25fd742d3d78d10c76171496f
lissiemarie <elyssamunden@gmail.com>
2022-12-23 11:11:13 -0800

    moved showTempSearch() into global scope

--- src/index.js
+++ src/index.js
@@ -24,50 +24,8 @@ if (currentMinutes >= 10) {
   dayTime.innerHTML = `${currentDay} ${currentHour}:0${currentMinutes}`;
 }
 
-//When current button is clicked:
-const getPosition = (position) => {
-  const showTempSearch = (response) => {
-    //temp
-    let temp = document.querySelector(".temp");
-    let currentTemp = Math.round(response.data.main.temp);
-    temp.innerHTML = `${currentTemp}°C`;
-    //city
-    let city = document.querySelector("h1");
-    let currentCity = response.data.name;
-    city.innerHTML = currentCity;
-    //weather
-    let weather = document.querySelector(".current-weather");
-    let currentWeather = response.data.weather.description;
-    weather.innerHTML = currentWeather;
-    //humidity
-    let humidity = document.querySelector(".humidity");
-    let currentHumidity = response.data.main.humidity;
-    humidity.innerHTML = currentHumidity;
-    //wind
-    let wind = document.querySelector(".wind");
-    let currentWind = Math.round(response.data.wind.speed);
-    wind.innerHTML = currentWind;
-  };
-  let apiKey = "7d478f69e1b2f5d563653f13f5f91d76";
-  let lat = position.coords.latitude;
-  let long = position.coords.longitude;
-  let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${apiKey}&units=metric`;
-  axios.get(url).then(showTempSearch);
-};
-
-const currentGetPosition = (event) => {
-  event.preventDefault();
-  navigator.geolocation.getCurrentPosition(getPosition);
-};
-
-let currentButton = document.querySelector("#current-button");
-currentButton.addEventListener("click", currentGetPosition);
-
-//when search button is clicked
-
-const getSearchedCity = (event) => {
-  event.preventDefault();
-  const showTempSearch = (response) => {
+//All doc selectors
+const showTempSearch = (response) => {
     //temp
     let temp = document.querySelector(".temp");
     let currentTemp = Math.round(response.data.main.temp);
@@ -89,6 +47,28 @@ const getSearchedCity = (event) => {
     let currentWind = Math.round(response.data.wind.speed);
     wind.innerHTML = `Wind: ${currentWind}`;
   };
+
+//When current button is clicked:
+const getPosition = (position) => {
+  let apiKey = "7d478f69e1b2f5d563653f13f5f91d76";
+  let lat = position.coords.latitude;
+  let long = position.coords.longitude;
+  let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${apiKey}&units=metric`;
+  axios.get(url).then(showTempSearch);
+};
+
+const currentGetPosition = (event) => {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(getPosition);
+};
+
+let currentButton = document.querySelector("#current-button");
+currentButton.addEventListener("click", currentGetPosition);
+
+//when search button is clicked
+
+const getSearchedCity = (event) => {
+  event.preventDefault();
   let cityInput = document.querySelector("#city-input").value;
   let apiKey = "7d478f69e1b2f5d563653f13f5f91d76";
   let url = `https://api.openweathermap.org/data/2.5/weather?q=${cityInput}&appid=${apiKey}&units=metric`;
